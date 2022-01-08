057d78b9c1fedf8d655f7d0bbfa19170df6a0730
Kleber Cabarite da Silva <kleber.cabaritesilva@everis.nttdata.com>
2022-01-07 23:51:49 -0300

    Acrescentado o User-Agent nos testes

--- src/test/java/testes/TesteApiDummy.java
+++ src/test/java/testes/TesteApiDummy.java
@@ -1,11 +1,12 @@
 package testes;
 
-import org.junit.Assert;
+import java.io.IOException;
+import java.net.URL;
+import javax.net.ssl.HttpsURLConnection;
 import org.junit.FixMethodOrder;
 import org.junit.runners.MethodSorters;
+import org.testng.Assert;
 import org.testng.annotations.Test;
-
-//import org.junit.Test;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
@@ -13,37 +14,51 @@ import net.minidev.json.JSONObject;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class TesteApiDummy {
-
-
+	
 		@Test()
-		public void test1_getAllEmployee() throws Exception {
-		
-			RestAssured.baseURI ="https://dummy.restapiexample.com/api/v1";
-			RequestSpecification request = RestAssured.given();
+		public void test2_getAllEmployees() throws IOException, Exception {
 			
+			String url = "https://dummy.restapiexample.com/api/v1";	
+			URL obj = new URL(url);
+		
+			RestAssured.baseURI = url;
+			RequestSpecification request = RestAssured.given();
+		
 			Thread.sleep(60000);
+		
+			HttpsURLConnection hc = (HttpsURLConnection) obj.openConnection(); 
+			hc.setRequestProperty("User-Agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36");
+			hc.connect();
+		
 			JSONObject requestParams = new JSONObject();	
 			request.body(requestParams.toJSONString());
 			Response response = request.get("/employees");
-	
+
 			int statusCode = response.getStatusCode();
 			Assert.assertEquals(statusCode, 200); 
-		
+				
 			String jsonString = response.asString();
 			System.out.println(jsonString);
 			Assert.assertEquals(jsonString.contains("Successfully! All records has been fetched."), true);
-		}
-		
+		}       
+
 		@Test
-		public void test2_postEmployee() throws Exception {
+		public void test1_postEmployee() throws IOException, Exception {
 			
-			RestAssured.baseURI ="https://dummy.restapiexample.com/api/v1";
+			String url = "https://dummy.restapiexample.com/api/v1";	
+			URL obj = new URL(url);
+			
+			RestAssured.baseURI = url;
 			RequestSpecification request = RestAssured.given();
 
-			JSONObject requestParams = new JSONObject();
-			
 			Thread.sleep(60000);
 			
+			HttpsURLConnection hc = (HttpsURLConnection) obj.openConnection(); 
+	        hc.setRequestProperty("User-Agent","PostmanRuntime/7.28.4");
+	        hc.connect();
+	        
+			JSONObject requestParams = new JSONObject();
+
 			requestParams.put("employee_name", "Jose");
 			requestParams.put("employee_salary", "31222");
 			requestParams.put("employee_age", "38");
@@ -65,10 +80,18 @@ public class TesteApiDummy {
 		
 			int id = 1;	
 			
-			RestAssured.baseURI ="https://dummy.restapiexample.com/api/v1";
+			String url = "https://dummy.restapiexample.com/api/v1";	
+			URL obj = new URL(url);
+			
+			RestAssured.baseURI = url;
 			RequestSpecification request = RestAssured.given();
-		
+
 			Thread.sleep(60000);
+			
+			HttpsURLConnection hc = (HttpsURLConnection) obj.openConnection(); 
+	        hc.setRequestProperty("User-Agent","Java/1.8.0_301");
+	        hc.connect();
+	        
 			JSONObject requestParams = new JSONObject();	
 			request.body(requestParams.toJSONString());
 			Response response = request.get("/employee/" + id);
@@ -85,12 +108,20 @@ public class TesteApiDummy {
 		@Test()
 		public void test4_putEmployee() throws Exception {
 			
-			int id = 100;
+			int id = 21;
 
-			RestAssured.baseURI ="https://dummy.restapiexample.com/api/v1";
-			RequestSpecification request = RestAssured.given();
+			String url = "https://dummy.restapiexample.com/api/v1";	
+			URL obj = new URL(url);
 			
+			RestAssured.baseURI = url;
+			RequestSpecification request = RestAssured.given();
+
 			Thread.sleep(60000);
+			
+			HttpsURLConnection hc = (HttpsURLConnection) obj.openConnection(); 
+	        hc.setRequestProperty("User-Agent","Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36");
+	        hc.connect();
+	        
 			JSONObject requestParams = new JSONObject();
 			requestParams.put("employee_name", "Kleber");
 			requestParams.put("employee_salary", "3133");
@@ -112,9 +143,17 @@ public class TesteApiDummy {
 			
 			int id = 3;
 			
-			Thread.sleep(60000);	
-			RestAssured.baseURI = "https://dummy.restapiexample.com/api/v1";
-			RequestSpecification request = RestAssured.given();	
+			String url = "https://dummy.restapiexample.com/api/v1";	
+			URL obj = new URL(url);
+			
+			RestAssured.baseURI = url;
+			RequestSpecification request = RestAssured.given();
+
+			Thread.sleep(60000);
+			
+			HttpsURLConnection hc = (HttpsURLConnection) obj.openConnection(); 
+	        hc.setRequestProperty("User-Agent", "Java/1.7.0_11");
+	        hc.connect();
 				
 			request.header("Content-Type", "application/json");	
 			
